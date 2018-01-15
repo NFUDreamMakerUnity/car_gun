fba14fe35394f8a9a201dcbb2041297687998603
user-PC\user <wueric0622@gmail.com>
2018-01-15 13:56:52 +0800

    VRTK asset import 程式範例檔案

---
+++ Assets/Scripts/example.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class example : MonoBehaviour {
+
+	public static example instence;
+
+	void Awack()
+	{
+		if (instence == null) {
+			instence = this;
+		} else {
+			DestroyImmediate (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

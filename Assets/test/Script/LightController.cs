15c5c9d4602791b7975c50e58a5d2140c341ab92
MSI\MSl <sss7634h9@gmail.com>
2018-01-16 15:38:38 +0800

    車2

---
+++ Assets/test/Script/LightController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    bool lightSwitch = false;
+    [SerializeField] Renderer light_1;
+    [SerializeField] Renderer light_2;
+
+    [SerializeField] Material lightOff;
+    [SerializeField] Material lightOn;
+
+    [SerializeField] Light L_one;
+    [SerializeField] Light L_two;
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            LightSwitch();
+        }
+
+        Light();        
+    }
+
+    void LightSwitch()
+    {
+        lightSwitch = !lightSwitch;
+    }
+
+    void Light()
+    {
+        if (lightSwitch)
+        {
+            light_1.material = lightOn;
+            light_2.material = lightOn;
+            L_one.enabled = true;
+            L_two.enabled = true;
+        }
+        else if (!lightSwitch)
+        {
+            light_1.material = lightOff;
+            light_2.material = lightOff;
+            L_one.enabled = false;
+            L_two.enabled = false;
+        }
+
+    }
+
+}

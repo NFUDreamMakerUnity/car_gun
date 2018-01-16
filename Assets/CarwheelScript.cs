fc4f466214ae238cada0fdd27a6a9e7dffa64a4e
user-PC\user <wueric0622@gmail.com>
2018-01-16 13:54:33 +0800

    SteamVR插件 方向盤與車內VR功能

---
+++ Assets/CarwheelScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarwheelScript : MonoBehaviour {
+
+	Rigidbody rb;
+	HingeJoint hj;
+	[SerializeField] float wheelRot;
+	
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		hj = GetComponent<HingeJoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		wheelRot = hj.angle/90;
+	}
+}
---
+++ Assets/SteamVR/PostionReset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+
+public class PostionReset : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		//UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
+
